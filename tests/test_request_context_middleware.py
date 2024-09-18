a7eadfdacb4f34146696147b2ca94777600a3f5b
oschulz <oschulz@samhammer.de>
2024-09-18 10:22:20 +0200

    KIT-3695 update unit test

--- tests/test_request_context_middleware.py
+++ tests/test_request_context_middleware.py
@@ -1,4 +1,8 @@
+from typing import Union, Any
+
+import pytest
 from starlette.applications import Starlette
+from starlette.responses import JSONResponse
 from starlette.types import Scope
 
 from sag_py_fastapi_request_id import request_context
@@ -42,3 +46,34 @@ def test_call_request_id_for_http() -> None:
 
     # Assert
     assert len(request_context.get_request_id()) == 32
+
+
+@pytest.fixture
+async def receive() -> Any:
+    async def receive() -> dict[str, Union[bool, str, bytes]]:
+        return {"type": "http.request", "body": b"", "more_body": False}
+
+    return receive
+
+
+@pytest.fixture
+async def send() -> Any:
+    async def send(_: Any) -> None:
+        return
+
+    return send
+
+
+@pytest.mark.asyncio
+async def test__call__(receive: Any, send: Any) -> None:
+    # Arrange
+    async def test_app(_: Any, receive: Any, send: Any) -> JSONResponse:
+        return JSONResponse({"message": "Hello, World!"})
+
+    scope: Scope = {"type": "http"}
+    middleware = RequestContextMiddleware(test_app)  # type: ignore
+
+    # Act
+    await middleware(scope, receive, send)
+
+    # Assert
